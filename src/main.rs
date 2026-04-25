1e7c4c82d861c530020480861d55a916dcedac30
sheumais <88064574+sheumais@users.noreply.github.com>
2026-04-25 19:06:18 +1000

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use yew_router::prelude::*;
+use yew::prelude::*;
+
+#[derive(Clone, Routable, PartialEq)]
+enum Route {
+    #[at("/")]
+    Home,
+    #[at("/post/:id")]
+    Post { id: String },
+    #[at("/*path")]
+    Misc { path: String },
+}
+
+fn switch(route: Route) -> Html {
+    match route {
+        Route::Home => html! { <h1>{ "Home" }</h1> },
+        Route::Post { id } => html! {<p>{format!("You are looking at Post {}", id)}</p>},
+        Route::Misc { path } => html! {<p>{format!("Matched some other path: {}", path)}</p>},
+    }
+}
+
+#[component(Main)]
+fn app() -> Html {
+    html! {
+        <BrowserRouter>
+            <Switch<Route> render={switch} />
+        </BrowserRouter>
+    }
+}
+
+fn main() {
+    yew::Renderer::<Main>::new().render();
+}
